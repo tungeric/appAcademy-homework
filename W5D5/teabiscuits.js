2cb64393e40de73f0b575626b7d518a6a2f87a81
Eric Tung <eric.p.tung@gmail.com>
2017-08-31 23:40:30 -0700

    javascript hw

---
+++ W5D5/teabiscuits.js
@@ -0,0 +1,49 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  // it's okay if this part is magic; it just says that we want to
+  // 1. output the prompt to the standard output (console)
+  // 2. read input from the standard input (again, console)
+
+  input: process.stdin,
+  output: process.stdout
+});
+
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits () {
+  reader.question('Would you like some tea?', function (res) {
+    console.log(`You replied ${res}.`);
+    reader.question('Would you like some biscuits?', function (res2) {
+      console.log(`You replied ${res2}.`);
+
+      const first = (res === 'yes') ? 'do' : 'don\'t';
+      const second = (res2 === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+
+// function teaAndBiscuits () {
+//   let first, second;
+//   reader.question('Would you like some tea?', (res) => {
+//     first = res;
+//     console.log(`You replied ${res}.`);
+//   });
+//
+//   reader.question('Would you like some biscuits?', (res) => {
+//     second = res;
+//     console.log(`You replied ${res}.`);
+//   });
+//
+//   const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+//   const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+//   console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+//   reader.close();
+// }
